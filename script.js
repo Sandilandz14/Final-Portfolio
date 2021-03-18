ee59d349291f6b630a3a4c08de9d25e9913d7848
Sandilandz14 <stukani100@gmail.com>
2021-03-18 16:01:44 +0200

    Added updated files

--- script.js
+++ script.js
@@ -34,19 +34,19 @@ $(document).ready(function () {
   });
 
   // typing text animation script
-  var typed = new Typed(".typing", {
-    strings: ["Software Developer", "Web Developer", "Programmer"],
-    typeSpeed: 100,
-    backSpeed: 60,
-    loop: true,
-  });
+  // var typed = new Typed(".typing", {
+  //   strings: ["Software Developer", "Web Developer", "Programmer"],
+  //   typeSpeed: 100,
+  //   backSpeed: 60,
+  //   loop: true,
+  // });
 
-  var typed = new Typed(".typing-2", {
-    strings: ["Software Developer", "Web Developer", "Programmer"],
-    typeSpeed: 100,
-    backSpeed: 60,
-    loop: true,
-  });
+  // var typed = new Typed(".typing-2", {
+  //   strings: ["Software Developer", "Web Developer", "Programmer"],
+  //   typeSpeed: 100,
+  //   backSpeed: 60,
+  //   loop: true,
+  // });
 
   // owl carousel script
   $(".carousel").owlCarousel({
